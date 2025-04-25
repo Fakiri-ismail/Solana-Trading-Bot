c1c443054eb10ff5fa52c83fe36b48e6f9fb95ea
FAKIRI <ismaiilfakirii@gmail.com>
2025-04-25 19:36:44 +0200

    add sell function

--- wallet/manager.py
+++ wallet/manager.py
@@ -1,16 +1,21 @@
-import requests
+import requests, base58, asyncio
 
+from solders.keypair import Keypair
 from solders.pubkey import Pubkey
 from solana.rpc.api import Client
 
-from global_config import HELIUS_RPC, SOL_URI, WSOL
+from exchanges.jupiter.swap import get_quote, build_transaction, send_transaction
+from wallet.helpers import get_signature_status
+from global_config import HELIUS_RPC, SOL_URI
+from global_config import USDC, WSOL
 
 
 class WalletManager():
-    def __init__(self, public_key: str, endpoint=SOL_URI):
+    def __init__(self, public_key: str, private_key:str, endpoint=SOL_URI):
         """Initialize the wallet manager"""
         self.client = Client(endpoint)
         self.public_key = public_key
+        self.payer = Keypair.from_bytes(base58.b58decode(private_key))
 
     def get_sol_balance(self) -> int:
         """Get wallet SOL balance"""
@@ -75,8 +80,39 @@ class WalletManager():
             if mint_or_symbol in [token["mint"], token["symbol"]]:
                 return token
 
-    def buy_token(self):
+    async def buy_token(self, mint: str, usd_amount: float, slippage: int = 500):
+        """
+        Buy a token using the Jupiter API
+        - mint: The mint address of the token to buy
+        - usd_amount: The USD amount of the token to buy
+        - param slippage: The slippage percentage (default is 5%)
+        :return: The transaction ID
+        """
         pass
 
-    def sell_token(self):
-        pass
+    async def sell_token(self, mint: str, pct_amount: float, slippage: int = 500):
+        """
+        Sell a token using the Jupiter API
+        - mint: The mint address of the token to sell
+        - pct_amount: The percentage of the token to sell (0-100)
+        - param slippage: The slippage percentage (default is 5%)
+        :return: The transaction ID
+        """
+        # Get the token info and calculate the amount to sell
+        token = self.get_token(mint)
+        amount = int(token["balance"] * (pct_amount / 100))
+
+        # Swap
+        quote = get_quote(mint, USDC, amount, slippage)
+        swap_transaction = build_transaction(self.public_key, quote)
+        tx_id = await send_transaction(self.payer, swap_transaction)
+        count, tx_status = 0, False
+        while not tx_status and count < 5:
+            tx_status = get_signature_status(tx_id)
+            count += 1
+            await asyncio.sleep(5)
+
+        return {
+            "transactionId": tx_id,
+            "status": tx_status
+        }
