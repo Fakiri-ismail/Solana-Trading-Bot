a44efc96dd17843c5a9551a3f5f8d92723aba335
FAKIRI <ismaiilfakirii@gmail.com>
2025-05-10 00:45:41 +0200

    update trading file

--- wallet/trading.py
+++ wallet/trading.py
@@ -1,58 +1,78 @@
 import asyncio
 from wallet.manager import WalletManager
 from exchanges.jupiter.price import getJupPrice
-from database.crud.wallet.wallet_tokens_ops import insert_wallet_token, delete_wallet_token
-from database.crud.wallet.wallet_tokens_ops import get_wallet_token, get_all_wallet_tokens
+from database.crud.wallet.wallet_tokens_ops import get_all_wallet_tokens
 from database.crud.wallet.trading_history_ops import create_trading_history
+from database.db_sync.cache_manager import load_wallet_cache, save_wallet_cache, sync_with_db
 from global_config import WALLET_PRIV_KEY, WALLET_PUB_KEY, WSOL, USDC
 
 
 async def main():
+    # Load the wallet cache
+    wallet_cache = load_wallet_cache()
+    if not wallet_cache:
+        db_wallet_tokens = get_all_wallet_tokens()
+        for token in db_wallet_tokens:
+            wallet_cache.append({
+                "mint": token.mint,
+                "symbol": token.symbol,
+                "purchase_price": float(token.purchase_price),
+                "usdt_value": float(token.usdt_value)
+            })
+
+    # Sol wallet
     my_wallet = WalletManager(WALLET_PUB_KEY, WALLET_PRIV_KEY)
     wallet_assets = my_wallet.get_assets()
     wallet_mints = [token.get("mint", "") for token in wallet_assets]
 
-    db_wallet_assets = get_all_wallet_tokens()
-    for db_token in db_wallet_assets:
-        if db_token.mint not in wallet_mints:
-            # Delete token from DB
-            delete_wallet_token(token_id=db_token.id)
+    # Update the wallet cache
+    # Remove tokens that are not in the wallet anymore
+    for token in wallet_cache:
+        if token["mint"] not in wallet_mints:
+            wallet_cache.remove(token)
 
-    db_wallet_assets = get_all_wallet_tokens()
-    db_mints = [db_token.mint for db_token in db_wallet_assets]
+    cache_mints = [t["mint"] for t in wallet_cache]
     for token in wallet_assets:
         token_actual_price = float(getJupPrice(token["mint"]).get(token["mint"], {}).get("price", 0))
         token_usd_value = token_actual_price * (token.get("balance", 0) / 10 ** token.get("decimals", 0))
-        # Check if token already exists in DB
-        if token["mint"] not in db_mints:
-            # Insert new token into DB
-            insert_wallet_token(
-                mint=token["mint"],
-                symbol=token["symbol"],
-                purchase_price=token_actual_price,
-                usdt_value=token_usd_value
-            )
+        # Check if token already exists in cache
+        if token["mint"] not in cache_mints:
+            # Add new tokens to the cache
+            wallet_cache.append({
+                "mint": token["mint"],
+                "symbol": token["symbol"],
+                "purchase_price": token_actual_price,
+                "usdt_value": token_usd_value
+            })
+
+        # Old tokens  
         else:
             if token["mint"] in [WSOL, USDC]:
                 continue
-            db_token = get_wallet_token(mint=token["mint"])
+            
+            cache_token = next((t for t in wallet_cache if t["mint"] == token["mint"]), None)
+            if cache_token:
+                threshold_upper = 1.8 * float(cache_token["purchase_price"])
+                threshold_lower = 0.5 * float(cache_token["purchase_price"])
 
-            threshold_upper = 1.8 * float(db_token.purchase_price)
-            threshold_lower = 0.5 * float(db_token.purchase_price)
+                if token_actual_price >= threshold_upper or token_actual_price <= threshold_lower:
+                    result = await my_wallet.sell_token(mint=token["mint"], pct_amount=100)
+                    if result.get("status"):
+                        create_trading_history(
+                            mint=token["mint"],
+                            symbol=token["symbol"], 
+                            usdt_value=token_usd_value,
+                            buy_price=float(cache_token["purchase_price"]),
+                            sell_price=token_actual_price
+                        )
+                        print(token["symbol"], "sold!")
+                    else:
+                        print(token["symbol"], ": Transaction failed!")
 
-            if token_actual_price >= threshold_upper or token_actual_price <= threshold_lower:
-                result = await my_wallet.sell_token(mint=token["mint"], pct_amount=100)
-                if result.get("status"):
-                    create_trading_history(
-                        mint=token["mint"],
-                        symbol=token["symbol"], 
-                        usdt_value=token_usd_value,
-                        buy_price=float(db_token.purchase_price),
-                        sell_price=token_actual_price
-                    )
-                    print(token["symbol"], "sold!")
-                else:
-                    print(token["symbol"], ": Transaction failed!")
+    save_wallet_cache(wallet_cache)
+
+    # Update the database
+    sync_with_db()
 
 
 asyncio.run(main())
