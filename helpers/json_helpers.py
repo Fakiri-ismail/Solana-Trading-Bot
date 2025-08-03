1f3f743beb8cb1e19eed3e471e68cb52890e5685
FAKIRI <ismaiilfakirii@gmail.com>
2025-08-04 00:18:52 +0200

    add json helper

---
+++ helpers/json_helpers.py
@@ -0,0 +1,27 @@
+import os, json
+
+
+def read_json_file(file_path):
+    if os.path.exists(file_path):
+        with open(file_path, "r") as f:
+            content = f.read()
+            if not content.strip():
+                return []
+            return json.loads(content)
+    return []
+
+
+def write_json_file(file_path, data):
+    with open(file_path, "w", encoding='utf-8') as f:
+        json.dump(data, f, indent=4)
+
+
+def add_json_record(file_path: str, record) -> None:
+    data = read_json_file(file_path)
+    
+    if type(record) is list:
+        data = data + record
+    else:
+        data.append(record)
+
+    write_json_file(file_path, data)
