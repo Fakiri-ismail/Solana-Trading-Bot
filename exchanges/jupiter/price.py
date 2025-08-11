2357f2e7fe26a6c97447537dc482efc948e2561f
FAKIRI <ismaiilfakirii@gmail.com>
2025-08-11 21:29:42 +0200

    add logging & update getJupPrice function

--- exchanges/jupiter/price.py
+++ exchanges/jupiter/price.py
@@ -1,7 +1,8 @@
-import requests
+import requests, logging
 from global_config import USDC
 
-def getJupPrice(mints: str, vs_token: str =USDC) -> dict:
+
+def getJupPrice(mint: str, vs_token: str =USDC) -> float:
     """
     Get the price of a token using the Jupiter API
     - mint: all mint address separated by a comma
@@ -9,21 +10,20 @@ def getJupPrice(mints: str, vs_token: str =USDC) -> dict:
     :return: The price of the token in USD
     """
         
-    url = f'https://lite-api.jup.ag/price/v2?ids={mints}&vsToken={vs_token}'
+    url = f'https://lite-api.jup.ag/price/v2?ids={mint}&vsToken={vs_token}'
     try:
         response = requests.get(url)
         response.raise_for_status()
-        return response.json().get("data", {})
+        price = response.json().get("data", {}).get(mint, {}).get("price", 0) 
+        return float(price)
     except requests.RequestException as e:
-        print(f"JUP API : Error fetching '{mints}' price\n >> {e}")
-        return {}
+        logging.error(f"JUP API : Error fetching '{mint}' price\n >> {e}")
+        return 0
 
 
 if __name__ == "__main__":
     # Example usage
     from global_config import WSOL
-    price = float(getJupPrice(WSOL).get(WSOL, {}).get("price", 0))
-    if not price:
-        raise Exception(f"Price not found for {WSOL}")
+    price = getJupPrice(WSOL)
     print(price)
     
