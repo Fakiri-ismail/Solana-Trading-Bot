a59c80d14fc451b934cfc70e5adb0772706178e0
FAKIRI <ismaiilfakirii@gmail.com>
2025-08-11 22:21:51 +0200

    change logging level from DEBUG to INFO

--- helpers/utils.py
+++ helpers/utils.py
@@ -12,7 +12,7 @@ def setup_logging(log_file):
     logging.basicConfig(
         filename=log_file,
         filemode='a',
-        level=logging.DEBUG,
+        level=logging.INFO,
         format='%(asctime)s - %(levelname)s - %(filename)s:%(lineno)d - %(message)s',
         datefmt='%Y-%m-%d %H:%M:%S'
     )
