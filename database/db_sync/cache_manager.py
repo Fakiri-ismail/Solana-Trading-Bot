c61c7dac5ef3044ef9032366d9de44cdc744ae95
FAKIRI <ismaiilfakirii@gmail.com>
2025-09-02 17:36:56 +0200

    add update_wallet_cache function

--- database/db_sync/cache_manager.py
+++ database/db_sync/cache_manager.py
@@ -1,33 +1,83 @@
 import os, json
 from datetime import datetime
 from helpers import json_helpers
+from wallet.manager import WalletManager
+from exchanges.jupiter.price import getJupPrice
 from database.crud.wallet import wallet_tokens_ops
 from database.crud.tokens import top_trading_tokens_ops
+from global_config import WALLET_PRIV_KEY, WALLET_PUB_KEY
 
 
 WALLET_CACHE_FILE = "database/db_sync/wallet_cache.json"
 TOP_TRADING_CACHE_FILE = "database/db_sync/top_trading_cache.json"
 LAST_UPDATE_FILE = "database/db_sync/last_update.json"
 
-# Wallet chache
+# Wallet cache
 def load_wallet_cache() -> list:
-    wallet_data = json_helpers.read_json_file(WALLET_CACHE_FILE)
-    if not wallet_data:
+    wallet_cache = json_helpers.read_json_file(WALLET_CACHE_FILE)
+    if not wallet_cache:
         # Get wallet tokens from the database
-        wallet_data = [
+        wallet_cache = [
             {
                 "mint": token.mint,
                 "symbol": token.symbol,
+                "balance": int(token.balance),
                 "buy_price": float(token.buy_price),
                 "usdt_value": float(token.usdt_value)
             }
             for token in wallet_tokens_ops.get_all_wallet_tokens()
         ]
-        save_wallet_cache(wallet_data)
-    return wallet_data
 
-def save_wallet_cache(wallet_data):
-    json_helpers.write_json_file(WALLET_CACHE_FILE, wallet_data)
+    return wallet_cache
+
+def update_wallet_cache(wallet_cache: list):
+    my_wallet = WalletManager(WALLET_PUB_KEY, WALLET_PRIV_KEY)
+    wallet_assets = my_wallet.get_assets()
+    wallet_mints = [token["mint"] for token in wallet_assets]
+    cache_mints = [token["mint"] for token in wallet_cache]
+
+    # Remove tokens that are not in the wallet anymore
+    for token in wallet_cache:
+        if token["mint"] not in wallet_mints:
+            wallet_cache.remove(token)
+
+    # Add new tokens and Update existing ones
+    for token in wallet_assets:
+        if token["mint"] in cache_mints:
+            cache_token = next((t for t in wallet_cache if t["mint"] == token["mint"]), None)
+            # Check Balance
+            if cache_token['balance'] != token['balance']:
+                actual_price = getJupPrice(token["mint"])
+                if not actual_price:
+                    continue
+
+                if cache_token['balance'] < token['balance']:
+                    # Calculate average buy price
+                    added_balance = token['balance'] - cache_token['balance']
+                    new_buy_price = (cache_token['balance'] * cache_token['buy_price'] + added_balance * actual_price) / token['balance']
+                else:
+                    new_buy_price = actual_price
+
+                cache_token['balance'] = token['balance']
+                cache_token['buy_price'] = new_buy_price
+                cache_token['usdt_value'] = new_buy_price * (token['balance'] / 10 ** token["decimals"])
+        else:
+            buy_price = getJupPrice(token["mint"])
+            if not buy_price:
+                continue
+            wallet_cache.append({
+                "mint": token["mint"],
+                "symbol": token["symbol"],
+                "balance": token["balance"],
+                "buy_price": buy_price,
+                "usdt_value": buy_price * (token['balance'] / 10 ** token["decimals"])
+            })
+
+    save_wallet_cache(wallet_cache)
+    return wallet_cache
+
+def save_wallet_cache(wallet_cache):
+    json_helpers.write_json_file(WALLET_CACHE_FILE, wallet_cache)
 
 # Top Trading pools cache
 def load_top_trading_pools_cache():
@@ -65,7 +115,7 @@ def sync_wallet_with_db():
     """
     wallet_last_sync = get_last_sync_time('wallet')
     if wallet_last_sync:
-        if (datetime.now() - wallet_last_sync).total_seconds() > 3600:
+        if (datetime.now() - wallet_last_sync).total_seconds() > 10800:  # 3 hours
             # Load the wallet cache
             wallet_cache = load_wallet_cache()
 
@@ -77,6 +127,7 @@ def sync_wallet_with_db():
                 wallet_tokens_ops.insert_wallet_token(
                     mint=token["mint"],
                     symbol=token["symbol"],
+                    balance=token["balance"],
                     buy_price=token["buy_price"],
                     usdt_value=token["usdt_value"])
             
